ce3b18a59d957b00c6b2bb6fe792a32cfd2cba85
Matheus Caetano <caetanomatheus876@gmail.com>
2021-03-16 12:01:29 -0300

    Routes configuration

---
+++ lib/router/routes.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import 'package:proffy/src/view/pages/app_loader/app_loader_page.dart';
+
+const String AppLoader = '/app-loader';
+
+final routes = [
+  GetPage(name: AppLoader, page: () => AppLoaderPage()),
+];
--- lib/src/view/proffy.dart
+++ lib/src/view/proffy.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'package:proffy/src/view/utils/app_theme.dart';
+import 'package:proffy/router/routes.dart' as router;
 
 class Proffy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Proffy',
-      debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      home: Scaffold(body: Center(child: Text('Proffy APP'))),
+      debugShowCheckedModeBanner: false,
+      initialRoute: router.AppLoader,
+      getPages: router.routes,
     );
   }
 }
