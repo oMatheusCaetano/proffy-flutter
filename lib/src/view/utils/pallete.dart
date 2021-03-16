1adc520bc4006be138480840ad25822d60d376df
Matheus Caetano <caetanomatheus876@gmail.com>
2021-03-16 09:52:58 -0300

    Created collor pallete file

---
+++ lib/src/view/utils/pallete.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class Pallete {
+  static final Color primary = Color(0xFF8257E5);
+  static final Color secondary = Color(0xFF24EF7F);
+  static final Color background = Color(0xFFF0F0F7);
+  static final Color inputBackground = Color(0xFFE6E6F0);
+  static final Color border = Color(0xFF9C98A6);
+  static final Color text = Color(0xFF6A6180);
+  static final Color textInPrimary = Color(0xFFD4C2FF);
+  static final Color textInPrimary2 = Color(0xFFFFFFFF);
+  static final Color placeholder = Color(0xFFD4C2FF);
+  static final Color title = Color(0xFF32264D);
+  static final Color error = Color(0xFFE33D3D);
+}
