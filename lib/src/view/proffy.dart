d413e822c4580d8839a2b31bd9d51f9be02fd9ea
Matheus Caetano <caetanomatheus876@gmail.com>
2021-03-16 09:49:28 -0300

    Created Proffy Root Widget

---
+++ lib/src/view/proffy.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Proffy extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Proffy',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Proffy APP'))),
+    );
+  }
+}
