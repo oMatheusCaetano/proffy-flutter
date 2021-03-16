2d754e542021037982da95c239c092e337c98a6b
Matheus Caetano <caetanomatheus876@gmail.com>
2021-03-16 09:53:50 -0300

    Updated collor pallete file

--- lib/src/view/utils/pallete.dart
+++ lib/src/view/utils/pallete.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 abstract class Pallete {
-  static final Color primary = Color(0xFF8257E5);
-  static final Color secondary = Color(0xFF24EF7F);
-  static final Color background = Color(0xFFF0F0F7);
-  static final Color inputBackground = Color(0xFFE6E6F0);
-  static final Color border = Color(0xFF9C98A6);
-  static final Color text = Color(0xFF6A6180);
-  static final Color textInPrimary = Color(0xFFD4C2FF);
-  static final Color textInPrimary2 = Color(0xFFFFFFFF);
-  static final Color placeholder = Color(0xFFD4C2FF);
-  static final Color title = Color(0xFF32264D);
-  static final Color error = Color(0xFFE33D3D);
+  static const Color Primary = Color(0xFF8257E5);
+  static const Color Secondary = Color(0xFF24EF7F);
+  static const Color Background = Color(0xFFF0F0F7);
+  static const Color InputBackground = Color(0xFFE6E6F0);
+  static const Color Border = Color(0xFF9C98A6);
+  static const Color Text = Color(0xFF6A6180);
+  static const Color TextInPrimary = Color(0xFFD4C2FF);
+  static const Color TextInPrimary2 = Color(0xFFFFFFFF);
+  static const Color Placeholder = Color(0xFFD4C2FF);
+  static const Color Title = Color(0xFF32264D);
+  static const Color Error = Color(0xFFE33D3D);
 }
