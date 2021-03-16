bce976dc5d7987d812dc3c8f9c53f2f8e3f6d115
Matheus Caetano <caetanomatheus876@gmail.com>
2021-03-16 10:00:39 -0300

    Used application global theme

--- lib/src/view/proffy.dart
+++ lib/src/view/proffy.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:proffy/src/view/utils/app_theme.dart';
+
 class Proffy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Proffy',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.theme,
       home: Scaffold(body: Center(child: Text('Proffy APP'))),
     );
   }
